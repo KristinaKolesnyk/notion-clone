6ac310b40956786d5552d605e801cb8c56d01f9f
KristinaKolesnyk <kkristina.kolesnyk@gmail.com>
2025-11-14 22:36:36 -0500

    checking the change

--- src/Page/useFocusedNodeIndex.ts
+++ src/Page/useFocusedNodeIndex.ts
@@ -1,9 +1,24 @@
 import type {NodeData} from "../utils/types.ts";
+import {useEffect, useState, Dispatch, SetStateAction} from "react";
 
 type UseFocusedNodeIndex = {
     nodes: NodeData[];
 }
 
-export const useFocusedNodeIndex = ({nodes}: UseFocusedNodeIndex) => {
+export const useFocusedNodeIndex = ({nodes}: UseFocusedNodeIndex): [number, Dispatch<SetStateAction<number>>] => {
+    const [focusedNodeIndex, setFocusedNodeIndex] = useState(0);
+    useEffect(() => {
+        const onKeyDown = (event: KeyboardEvent) => {
+            if (event.key === "ArrowUp") {
+                setFocusedNodeIndex(index => Math.max(index - 1, 0));
+            }
+            if (event.key === "ArrowDown") {
+                setFocusedNodeIndex(index => Math.min(index + 1, nodes.length - 1));
+            }
+        }
+        document.addEventListener("keydown", onKeyDown);
 
+        return () => document.removeEventListener("keydown", onKeyDown);
+    }, [nodes]);
+    return [focusedNodeIndex, setFocusedNodeIndex];
 }
