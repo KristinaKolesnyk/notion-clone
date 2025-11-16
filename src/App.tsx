ba7f70f60d77911517f8610b985e6aa17460f5af
KristinaKolesnyk <kkristina.kolesnyk@gmail.com>
2025-11-16 09:48:55 -0500

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 import './App.css'
 import {Page} from "./Page/Page";
-import {createPage} from "./utils/createPage";
 import {AppStateProvider} from "./state/AppStateContext";
 import {Route, Routes} from 'react-router-dom'
 import {Auth} from "./auth/Auth.tsx";
 import {Private} from "./auth/Private.tsx";
 
-const initialState = createPage();
-
 function App() {
     return (
         <Routes>
-            <Route path="/auth" element={<Auth/>}/>
-            <Route path="/:id" element={
-                <Private component={
-                    <AppStateProvider initialState={initialState}>
-                        <Page/>
-                    </AppStateProvider>
+            <Route path="/auth" element={<Auth />} />
+            <Route
+                path="/:id"
+                element={
+                    <Private
+                        component={
+                            <AppStateProvider >
+                                <Page />
+                            </AppStateProvider>
+                        }
+                    />
                 }
-                />
-            }/>
-            <Route path="/" element={
-                <Private component={
-                    <AppStateProvider initialState={initialState}>
-                        <Page/>
-                    </AppStateProvider>}/>
-            }/>
-
+            />
+            <Route
+                path="/"
+                element={
+                    <Private
+                        component={
+                            <AppStateProvider >
+                                <Page />
+                            </AppStateProvider>
+                        }
+                    />
+                }
+            />
         </Routes>
-    )
+    );
 }
 
-export default App
+export default App;
